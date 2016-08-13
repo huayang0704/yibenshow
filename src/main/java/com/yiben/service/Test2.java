f0cb33e392fd94da3a6d16ed8e1adcfcdf530d0e
huayang <huayang0704@gmail.com>
2016-08-13 08:14:16 +0800

    a

---
+++ src/main/java/com/yiben/service/Test2.java
@@ -0,0 +1,8 @@
+package com.yiben.service;
+
+/**
+ * Created by huayang on 16/8/11.
+ */
+public class
+        Test2 {
+}
---
+++ src/main/java/com/yiben/servlet/Test.java
@@ -0,0 +1,7 @@
+package com.yiben.servlet;
+
+/**
+ * Created by huayang on 16/8/11.
+ */
+public class Test {
+}
